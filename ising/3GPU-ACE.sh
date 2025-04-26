3b082a00bfac289e9685370074f5c15884ca45b6
Aryan Blaauw <ablaauw@gmx.net>
2025-04-26 16:11:07 +0200

    Update 3GPU-ACE.sh

--- ising/3GPU-ACE.sh
+++ ising/3GPU-ACE.sh
@@ -3,6 +3,7 @@
 export QRACK_OCL_DEFAULT_DEVICE=0
 export QRACK_MAX_PAGING_QB=28
 export QRACK_QTENSORNETWORK_THRESHOLD_QB=1
+export QRACK_QBDT_MAX_ALLOC_MB=10000
 
 cd /pyqrack-examples/ising
 python3 ising_ace.py
