8db647061ce14994cdbdeebe4cc345a850a4c4a0
Aryan Blaauw <ablaauw@gmx.net>
2025-05-28 19:53:45 +0200

    Update ising_ace_depth_series.sh

--- ising/ising_ace_depth_series.sh
+++ ising/ising_ace_depth_series.sh
@@ -6,4 +6,24 @@ export QRACK_QTENSORNETWORK_THRESHOLD_QB=-1
 export QRACK_OCL_DEFAULT_DEVICE=2
 export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.1464466
 
-python3 ising_ace_depth_full_series.py 28 28
+
+#!/bin/bash
+
+# This script runs the ising_ace_depth_series.py script
+# with the first parameter varying from 4 to 28,
+# and the second parameter (depth) fixed at 20.
+
+echo "Starting the script..."
+
+# Loop from 4 to 28 (inclusive)
+for i in $(seq 4 46)
+do
+  echo "Running forked with parameter: $i and depth: $i"
+
+  python3 ising_ace_depth_series.py "$i" "$i" > "$i".log &
+
+  sleep 10
+
+done
+
+echo "Script finished."
