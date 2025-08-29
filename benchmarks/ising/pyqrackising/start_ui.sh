9793a218d9f6d0d8710a7d9e52a9f79c95c10a30
Aryan Blaauw <ablaauw@gmx.net>
2025-08-29 21:46:36 +0200

    Update start_ui.sh

--- benchmarks/ising/pyqrackising/start_ui.sh
+++ benchmarks/ising/pyqrackising/start_ui.sh
@@ -9,11 +9,14 @@ pip install vedo pv tqdm basis-set-exchange
 
 # runs UIs for both sims
 
-echo "magnetisation ui"
-python3 magnetisation-iterations_ui.py &
+# echo "magnetisation ui"
+# python3 magnetisation-iterations_ui.py &
 
 echo "sqr magnetisation ui"
-python3 sqr_magnetisation-iterations_ui.py &
+python3 sqr_magnetisation-iterations_ui.py
+
+echo "render the movie from output"
+ffmpeg -framerate 24 -i TF%*.png -c:v libx264 -pix_fmt yuv420p tfim.mp4
 
 
 tail -f /dev/null 
