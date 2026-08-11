0960eff5898eb4feb0a1d0d6daccba74809da616
Aryan Blaauw <ablaauw@gmx.net>
2026-08-11 10:14:52 +0200

    Add XEB amplitude fetch script bro.sh

---
+++ bro.sh
@@ -0,0 +1,4 @@
+# XEB amplitude fetch
+
+python3 dcs_post_select_paralel.py nq70_depth70_checks27_doped_checks.qasm 
+python3 bro-xeb-viz.py probs nq70_depth70_checks27_doped_checks.qasm dcs_post_select_result.json --limit 1
