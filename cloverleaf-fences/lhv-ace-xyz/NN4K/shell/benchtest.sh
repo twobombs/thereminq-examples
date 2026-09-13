2e3957b480d1bdc6901b139a73914edbfe85399d
Aryan Blaauw <ablaauw@gmx.net>
2026-09-13 15:40:15 +0200

    Add benchtest.sh to run shell_dodec_kitaev.py scripts

---
+++ cloverleaf-fences/lhv-ace-xyz/NN4K/shell/benchtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 shell_dodec_kitaev.py
+
+python3 shell_dodec_kitaev.py --anneal 3000
+
+python3 shell_dodec_kitaev.py --shells 8 --core
+
+python3 shell_dodec_kitaev.py --hyperoctagon ../coils/K4-Chrystalstacks-Kitaev-single.py
